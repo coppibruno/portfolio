b0fb23c66b6a88c86ae262d05670056fc5f26cab
Bruno Coppi <brunocoppi130@gmail.com>
2022-08-11 09:48:39 -0300

    change profile image, add animate css, background black

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,3 +10,28 @@ import {
 
 // Or import just one
 // import Alert as Alert from '../node_modules/bootstrap/js/dist/alert';
+
+addEventListener('scroll', () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.7;
+  const skills = document.querySelector('.txtSkills');
+  const contact = document.querySelector('.txtFooter');
+
+  if (windowTop > skills.offsetTop) {
+    skills.classList.remove('opacity-0');
+    skills.classList.add('animate__animated');
+    skills.classList.add('animate__fadeInLeft');
+  } else {
+    skills.classList.add('opacity-0');
+    skills.classList.remove('animate__animated');
+    skills.classList.remove('animate__fadeInLeft');
+  }
+  if (windowTop > contact.offsetTop) {
+    contact.classList.remove('opacity-0');
+    contact.classList.add('animate__animated');
+    contact.classList.add('animate__fadeInLeft');
+  } else {
+    contact.classList.add('opacity-0');
+    contact.classList.remove('animate__animated');
+    contact.classList.remove('animate__fadeInLeft');
+  }
+});
